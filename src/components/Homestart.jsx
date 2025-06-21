9b32df9a226135c72ec6baa13ed787828ec441ee
Shashikanta Rout <116004580+SkRout8@users.noreply.github.com>
2025-06-21 16:29:45 +0530

    Update Homestart.jsx

--- src/components/Homestart.jsx
+++ src/components/Homestart.jsx
@@ -21,17 +21,17 @@ function Homestart() {
                         Your Electricity Bill is Burning a Hole in Your Wallet
                     </p>
                     <h1 className="text-[36px] md:text-5xl font-extrabold text-[#121827] leading-tight mb-1">
-                        SWITCH TO <span className="text-[#3a8c4a] italic font-extrabold">CLEAN</span>
+                        SWITCH TO <span className="text-[#FD8C4E] italic font-extrabold">SOLAR ENERGY</span>
                     </h1>
-                    <p className="text-[#3a8c4a] text-xs font-semibold uppercase mb-4">
+                    {/* <p className="text-[#3a8c4a] text-xs font-semibold uppercase mb-4">
                         cost-saving solar energy before the next rate hike.
-                    </p>
-                    <p className="text-gray-600 text-lg font-medium mb-8 leading-relaxed">
+                    </p> */}
+                    <p className="text-gray-600 text-sm font-medium mb-8 leading-relaxed">
                         Power your <span className="font-semibold">home</span> or business
                         <br />
                         with India’s most trusted solar experts.
                     </p>
-                    <button className="bg-[#3a8c4a] text-white font-semibold rounded-full py-3 px-8 w-full max-w-[320px] hover:bg-[#2f6f3a] transition">
+                    <button className="bg-[#E50C0C] text-white font-semibold rounded-full py-3 px-8 w-full max-w-[320px] hover:bg-[#2f6f3a] transition">
                         Get Free Solar Consultation
                     </button>
                 </div>
@@ -43,7 +43,7 @@ function Homestart() {
                         <img
                             src={solarGlobe}
                             alt="Solar globe"
-                            className="absolute ig top-[100px] left-[0px] sm:top-[100px] sm:left-[-50px] md:top-[200px] md:left-[-150px] lg:top-[320px] lg:left-[-125px] scale-[1.8] sm:scale-[1.7] md:scale-[1.8] lg:scale-[1] w-[400px] sm:w-[500px] md:w-[600px] lg:w-[700px] rotating-image z-10"
+                            className="absolute ig top-[100px] left-[0px] sm:top-[100px] sm:left-[-50px] md:top-[200px] md:left-[-150px] lg:top-[350px] lg:left-[-125px] scale-[1.8] sm:scale-[1.7] md:scale-[1.8] lg:scale-[1] w-[400px] sm:w-[500px] md:w-[600px] lg:w-[700px] rotating-image z-10"
                         />
  </div>
                     
@@ -51,7 +51,7 @@ function Homestart() {
                         <img
                             src={family}
                             alt="Smiling family"
-                            className="absolute top-3/4 left-[1000px] transform -translate-x-1/2 -translate-y-1/2 w-[220px] sm:w-[300px] md:w-[360px] lg:w-[400px] z-20"
+                            className="absolute top-[700px] left-[1050px] transform -translate-x-1/2 -translate-y-1/2 w-[220px] sm:w-[300px] md:w-[360px] lg:w-[400px] z-20"
                         />
                     </div>
                
