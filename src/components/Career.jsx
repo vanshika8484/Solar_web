2a90f519a88530bdfa58d6eaa216fa65bbf8618c
Shashikanta Rout <116004580+SkRout8@users.noreply.github.com>
2025-06-28 12:14:49 +0530

    Update Career.jsx

--- src/components/Career.jsx
+++ src/components/Career.jsx
@@ -81,7 +81,7 @@ const Career = () => {
   };
 
   return (
-    <div className="min-h-screen bg-gradient-to-br from-gray-50 to-gray-100 py-12 px-4 sm:px-6 lg:px-8">
+    <div className="min-h-screen bg-gradient-to-br from-gray-50 to-gray-100 py-12">
       {/* Hero Section */}
       <motion.div
         initial={{ opacity: 0, y: -20 }}
