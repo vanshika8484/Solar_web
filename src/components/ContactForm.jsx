c5811fe93e565d20b0a4e71d08fa349ef98b7faa
Shashikanta Rout <116004580+SkRout8@users.noreply.github.com>
2025-07-11 14:25:39 +0530

    Update ContactForm.jsx

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -49,7 +49,7 @@ const ContactForm = () => {
     <main className="bg-gray-900 px-4 sm:px-8 md:px-16 py-20 -mt-16 About max-h-[1250px] lg:max-h-[650px] md:max-h-[650px] xl:max-h-[650px] 2xl:max-h-[650px] ">
       <ToastContainer position="top-right" autoClose={3000} />
 
-      <div className="text-center max-w-4xl mx-auto mb-12 -mt-[70px]">
+      <div className="text-center max-w-4xl mx-auto mb-12 -mt-[50px]">
         <h2 className="text-xl md:text-3xl About text-white leading-snug About1">
           Bijli ka bill bhejiye and Solar Chacha se jaaniye<br />
           apne rooftop ke liye Best Solar Solution
