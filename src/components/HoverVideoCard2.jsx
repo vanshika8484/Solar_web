70ce46dc1ff58efee4a2cd1ab2a2029599f41e9c
Shashikanta Rout <116004580+SkRout8@users.noreply.github.com>
2025-07-11 17:13:12 +0530

    Update HoverVideoCard2.jsx

--- src/components/HoverVideoCard2.jsx
+++ src/components/HoverVideoCard2.jsx
@@ -4,37 +4,37 @@ import { ChevronLeft, ChevronRight } from "lucide-react";
 const teamMembers = [
   {
     name: 'Mark Makana',
-    role: 'Solar Specialist',
+    role: 'Mission & Vision',
     image: 'https://storage.googleapis.com/a1aa/image/249ae767-68b3-4bcd-1521-265ee80c5d7b.jpg',
     reel: 'https://res.cloudinary.com/dpopicwcb/video/upload/v1751351996/Founder_Mission_And_Vision_naxvpa.mp4',
   },
   {
     name: 'John Makana',
-    role: 'Solar Specialist',
+    role: 'Company is a Group Company',
     image: 'https://storage.googleapis.com/a1aa/image/ba2e056b-5797-4eaa-d680-7a78a84778c0.jpg',
     reel: 'https://res.cloudinary.com/dpopicwcb/video/upload/v1751351688/Founder_And_Friends_Company_Is_A_Group_Company_iaih5d.mp4',
   },
   {
     name: 'Arvvani Joe',
-    role: 'Solar Specialist',
+    role: 'Comapny & Growth',
     image: 'https://storage.googleapis.com/a1aa/image/d8ee8bcd-44eb-4126-73ad-adb857ac13a5.jpg',
     reel: 'https://res.cloudinary.com/dpopicwcb/video/upload/v1751351804/Founder_Company_Growth_pcuxia.mp4',
   },
   {
     name: 'Matchine Mue 1',
-    role: 'Solar Specialist',
+    role: 'Solving Government Propaganda',
     image: 'https://storage.googleapis.com/a1aa/image/5b4ec894-2c46-41d5-4e01-bd02f1629da7.jpg',
     reel: 'https://res.cloudinary.com/dpopicwcb/video/upload/v1751351804/Founder_And_Friends_Solving_Government_Propaganda_bc76ta.mp4',
   },
   {
     name: 'Matchine Mue 2',
-    role: 'Solar Specialist',
+    role: '25 Years into Energy',
     image: 'https://storage.googleapis.com/a1aa/image/5b4ec894-2c46-41d5-4e01-bd02f1629da7.jpg',
     reel: 'https://res.cloudinary.com/dpopicwcb/video/upload/v1751351837/Founder_1_va6xls.mp4',
   },
   {
     name: 'Matchine Mue 3',
-    role: 'Solar Specialist',
+    role: 'We Belive in Solution',
     image: 'https://storage.googleapis.com/a1aa/image/5b4ec894-2c46-41d5-4e01-bd02f1629da7.jpg',
     reel: 'https://res.cloudinary.com/dpopicwcb/video/upload/v1751351593/Founder_2_pu2tf6.mp4',
   },
@@ -95,10 +95,8 @@ const HoverVideoCard2 = () => {
                 />
               </div>
               <div className="mt-3 text-left">
-                <h3 className="text-md font-semibold text-gray-800">
-                  {member.name}
-                </h3>
-                <p className="text-sm text-gray-600">{member.role}</p>
+                
+                <p className="text-sm text-gray-600">Topic: {member.role}</p>
               </div>
             </div>
           ))}
