acddfeb76b0cee75bef7931bdc5c7ecbe04d7b49
Priyan-shu-sri <123482053+Priyan-shu-sri@users.noreply.github.com>
2025-07-15 16:58:26 +0530

    Update Gallery.jsx

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,11 +1,12 @@
 import { motion } from 'framer-motion';
-import e1 from "../Images/e1.JPG"
-import e2 from "../Images/e2.JPG"
-import e3 from "../Images/e3.JPG"
-import e4 from "../Images/e4.JPG"
-
-
-
+import e1 from "../Images/e1.JPG";
+import e2 from "../Images/e2.JPG";
+import e3 from "../Images/e3.JPG";
+import e4 from "../Images/e4.JPG";
+import e5 from "../Images/e5.JPG";
+import e6 from "../Images/e6.jpg";
+import e7 from "../Images/e7.jpg";
+import e8 from "../Images/e8.JPG";
 
 const images = [
   {
@@ -24,13 +25,30 @@ const images = [
     src: e4,
     alt: "Person looking at solar panels and wind turbine",
   },
+  {
+    src: e5,
+    alt: "Person looking at solar panels and wind turbine",
+  },
+  {
+    src: e6,
+    alt: "Person looking at solar panels and wind turbine",
+  },
+  {
+    src: e7,
+    alt: "Person looking at solar panels and wind turbine",
+  },
+  {
+    src: e8,
+    alt: "Person looking at solar panels and wind turbine",
+  },
 ];
 
 const Gallery = () => {
   return (
-    <main className="p-4 md:p-6 max-w-7xl mx-auto  md:px-12 lg:px-20 xl:px-32">
+    <main className="p-4 md:p-6 max-w-7xl mx-auto md:px-12 lg:px-20 xl:px-32">
       <div className="grid grid-cols-2 sm:grid-cols-4 gap-4">
-        {images.concat(images).map((image, index) => (
+        {/* Removed .concat(images) to avoid duplication */}
+        {images.map((image, index) => (
           <motion.div
             key={index}
             className="relative rounded-xl overflow-hidden group"
