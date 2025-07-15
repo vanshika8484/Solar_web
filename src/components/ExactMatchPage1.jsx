8c0a94fa84a24fc8f2f5cd455971d7aafb06c48a
Priyan-shu-sri <123482053+Priyan-shu-sri@users.noreply.github.com>
2025-07-15 11:02:59 +0530

    Update ExactMatchPage1.jsx

--- src/components/ExactMatchPage1.jsx
+++ src/components/ExactMatchPage1.jsx
@@ -9,22 +9,20 @@ import L6 from '../Images/P6.png';
 import L7 from '../Images/P7.jpg';
 import L8 from '../Images/P8.png';
 
-const logos = [L1, L2, L3, L4,L5,L6,L7,L8];
+const logos = [L1, L2, L3, L4, L5, L6, L7, L8];
 
 export default function ExactMatchPage1() {
   return (
-    <div className=" min-h-screen About1 -mt-24">
+    <div className="min-h-screen About1 -mt-24">
       {/* Heading */}
-      <h2 className="text-2xl sm:text-4xl  font-bold text-center pt-10 text-red-600">
+      <h2 className="text-2xl sm:text-4xl font-bold text-center pt-10 text-red-600">
         Our Clients
       </h2>
 
       {/* Animated Logos */}
       <div className="overflow-hidden py-10 sm:py-16 -mt-16">
         <div
-          className="flex space-x-10 px-4 sm:px-8"
-         
-          
+          className="flex space-x-10 px-4 sm:px-8 overflow-x-auto whitespace-nowrap"
         >
           {[...logos, ...logos].map((src, idx) => (
             <img
