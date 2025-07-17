d7460272664ea5965b6c4ed3824a579f7219c2dc
Shashikanta Rout <116004580+SkRout8@users.noreply.github.com>
2025-07-17 10:31:19 +0530

    Update SocialIcons.jsx

--- src/components/SocialIcons.jsx
+++ src/components/SocialIcons.jsx
@@ -4,9 +4,9 @@ import {
   FaFacebookF,
   FaXTwitter,
   FaLinkedinIn,
-  FaGithub,
   FaInstagram,
   FaYoutube,
+  FaWhatsapp,
 } from 'react-icons/fa6';
 
 const iconStyle = `w-8 h-8 sm:w-10 sm:h-10 rounded-full flex items-center justify-center text-white hover:scale-110 transition-transform duration-300 shadow-lg backdrop-blur-md`;
@@ -26,7 +26,7 @@ const SocialIcons = () => {
     <>
       {/* Left Side Icons - Facebook, Twitter (X), LinkedIn */}
       <div
-        className={`flex fixed top-1/2 left-3 -translate-y-1/2 flex-col gap-16 sm:gap-28 z-[9999] transition-opacity duration-500 ${
+        className={`flex fixed top-1/2 left-3 -translate-y-1/2 flex-col  gap-44 lg:gap-16 md:gap-16 xl:gap-16 2xl:gap-16 z-[9999] transition-opacity duration-500 ${
           showIcons ? 'opacity-100' : 'opacity-0'
         }`}
       >
@@ -59,20 +59,20 @@ const SocialIcons = () => {
         </a>
       </div>
 
-      {/* Right Side Icons - GitHub, Instagram, YouTube */}
+      {/* Right Side Icons - WhatsApp, Instagram, YouTube */}
       <div
-        className={`flex fixed top-1/2 right-3 -translate-y-1/2 flex-col gap-16 sm:gap-28 z-[9999] transition-opacity duration-500 ${
+        className={`flex fixed top-1/2 right-3 -translate-y-1/2 flex-col gap-44 lg:gap-16 md:gap-16 xl:gap-16 2xl:gap-16 z-[9999] transition-opacity duration-500 ${
           showIcons ? 'opacity-100' : 'opacity-0'
         }`}
       >
         <a
-          href="https://github.com"
+          href="https://wa.me/919310259325"
           target="_blank"
           rel="noopener noreferrer"
-          className={`${iconStyle} bg-gray-900`}
-          aria-label="GitHub"
+          className={`${iconStyle} bg-green-500`}
+          aria-label="WhatsApp"
         >
-          <FaGithub />
+          <FaWhatsapp />
         </a>
         <a
           href="https://www.instagram.com/divy.power/"
