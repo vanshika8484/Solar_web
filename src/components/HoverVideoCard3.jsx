68b4782adc08b03366d9f04bd00c50ca65737f44
Shashikanta Rout <116004580+SkRout8@users.noreply.github.com>
2025-07-22 16:22:44 +0530

    Update HoverVideoCard3.jsx

--- src/components/HoverVideoCard3.jsx
+++ src/components/HoverVideoCard3.jsx
@@ -1,29 +1,28 @@
 import React from "react";
-
 const reelsData = [
   {
     role: "Solar Specialist",
-    reel: "https://res.cloudinary.com/dpopicwcb/video/upload/v1751353920/Shruti_Growth_At_Divy_kbqin1.mp4",
+    reel: "https://res.cloudinary.com/drz2uocug/video/upload/v1753180799/Kaushlesh_Singh_Growth_At_Divyy_b7cnhq.mp4",
   },
   {
     role: "Solar Specialist",
-    reel: "https://res.cloudinary.com/dpopicwcb/video/upload/v1751354105/Riddhi_Growth_At_Divy_czkigm.mp4",
+    reel: "https://res.cloudinary.com/drz2uocug/video/upload/v1753180798/Kaushlesh_Growth_At_Divy_egqssb.mp4",
   },
   {
     role: "Solar Specialist",
-    reel: "https://res.cloudinary.com/dpopicwcb/video/upload/v1751354118/Rishika_Growth_At_Divy_r3mkm1.mp4",
+    reel: "https://res.cloudinary.com/drz2uocug/video/upload/v1753180798/Shruti_Growth_At_Divy_qolr0f.mp4",
   },
   {
     role: "Solar Specialist",
-    reel: "https://res.cloudinary.com/dpopicwcb/video/upload/v1751354236/Kaushlesh_Singh_Growth_At_Divyy_ztexms.mp4",
+    reel: "https://res.cloudinary.com/drz2uocug/video/upload/v1753180798/Asha_Growth_At_Divy_gklhuy.mp4",
   },
   {
     role: "Solar Specialist",
-    reel: "https://res.cloudinary.com/dpopicwcb/video/upload/v1751354256/Asha_Growth_At_Divy_mua1bn.mp4",
+    reel: "https://res.cloudinary.com/drz2uocug/video/upload/v1753180798/Rajeev_Arora_Growth_At_Divy_With_Updated_Tech_qmcrud.mp4",
   },
   {
     role: "Solar Specialist",
-    reel: "https://res.cloudinary.com/dpopicwcb/video/upload/v1751354256/Kaushlesh_Growth_At_Divy_jshtlv.mp4",
+    reel: "https://res.cloudinary.com/drz2uocug/video/upload/v1753180797/Riddhi_Growth_At_Divy_f94d23.mp4",
   },
 ];
 
