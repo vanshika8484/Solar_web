daf718230dffd6441ff9faa080d66ec5e5f26161
Priyan-shu-sri <123482053+Priyan-shu-sri@users.noreply.github.com>
2025-07-22 22:28:54 +0530

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -191,7 +191,7 @@ export default function About() {
         <ContactCards />
       </section>
 
-      <section className=" mt-20 lg:min-w-[1300px]  md:min-w-[1300px] xl:min-w-[1300px] 2xl:min-w-[1400px] ">
+      <section className=" mt-20 lg:min-w-[1600px]  md:min-w-[1600px] xl:min-w-[1600px] 2xl:min-w-[1600px] ">
         <Footer />
       </section>
 
