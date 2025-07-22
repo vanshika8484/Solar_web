8cefe2eabddcfce14ebc93e9b895865a048284b8
Shashikanta Rout <116004580+SkRout8@users.noreply.github.com>
2025-07-22 16:21:58 +0530

    Update FiguringOut.jsx

--- src/components/FiguringOut.jsx
+++ src/components/FiguringOut.jsx
@@ -5,25 +5,25 @@ const teamMembers = [
     name: 'Mark Makana',
     role: 'Solar Specialist',
     image: 'https://storage.googleapis.com/a1aa/image/249ae767-68b3-4bcd-1521-265ee80c5d7b.jpg',
-    reel: 'https://res.cloudinary.com/dpopicwcb/video/upload/v1750768191/Architecture_Interviews_Cosmopolitan_Development_2_zk4bie.mp4',
+    reel: 'https://res.cloudinary.com/drz2uocug/video/upload/v1753181164/Architecture_Interviews_Cosmopolitan_Development_2_zbw4s1.mp4',
   },
   {
     name: 'John Makana',
     role: 'Solar Specialist',
     image: 'https://storage.googleapis.com/a1aa/image/ba2e056b-5797-4eaa-d680-7a78a84778c0.jpg',
-    reel: 'https://res.cloudinary.com/dpopicwcb/video/upload/v1750768186/Founder_And_Friends_Growing_A_Business_wy02cz.mp4',
+    reel: 'https://res.cloudinary.com/drz2uocug/video/upload/v1753181164/Architecture_Interviews_Evolving_Tech_Using_Solar_1_r7eqrw.mp4',
   },
   {
     name: 'Arvvani Joe',
     role: 'Solar Specialist',
     image: 'https://storage.googleapis.com/a1aa/image/d8ee8bcd-44eb-4126-73ad-adb857ac13a5.jpg',
-    reel: 'https://res.cloudinary.com/dpopicwcb/video/upload/v1750768099/Architecture_Interviews_Challenges_Among_Industries_1_ozawpm.mp4',
+    reel: 'https://res.cloudinary.com/drz2uocug/video/upload/v1753181164/Founder_And_Friends_Solar_And_Hesitation_vzvned.mp4',
   },
   {
     name: 'Matchine Mue',
     role: 'Solar Specialist',
     image: 'https://storage.googleapis.com/a1aa/image/5b4ec894-2c46-41d5-4e01-bd02f1629da7.jpg',
-    reel: 'https://res.cloudinary.com/dpopicwcb/video/upload/v1750768044/Architecture_Interviews_Bbe_And_Benefits_1_rncmne.mp4',
+    reel: 'https://res.cloudinary.com/drz2uocug/video/upload/v1753181163/Architecture_Interviews_Challenges_Among_Industries_1_nbvvzb.mp4',
   },
 ];
 
