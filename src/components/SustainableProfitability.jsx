3c45a826ea67de78e035a628779d8c036c78ea6e
Shashikanta Rout <116004580+SkRout8@users.noreply.github.com>
2025-07-23 14:25:19 +0530

    Update SustainableProfitability.jsx

--- src/components/SustainableProfitability.jsx
+++ src/components/SustainableProfitability.jsx
@@ -3,7 +3,7 @@ import Footer from './Footer';
 
 const SustainableProfitability = () => {
   return (
-    <div className="max-w-5xl mx-auto px-4 py-12 text-gray-800 font-inter">
+    <div className="max-w-5xl mx-auto px-4 py-12 text-gray-800 About mt-28">
       <h1 className="text-3xl md:text-4xl font-bold text-brandBlue mb-4">
         Embracing Solar Energy: A Path to Sustainable Profitability
       </h1>
