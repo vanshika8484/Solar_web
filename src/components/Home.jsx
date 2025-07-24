3fb22a94da6ad946a8d0c5f5063d8c2fa407a6ad
Shashikanta Rout <116004580+SkRout8@users.noreply.github.com>
2025-07-24 20:50:52 +0530

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -137,7 +137,7 @@ function Home() {
       <section className=" h-72">
         <div className="text-center About1  py-6 mb-10  -mt-36 lg:-mt-44 md:-mt-40 ">
           <button className="bg-[#E50C0C] hover:bg-red-700 text-white font-bold py-3 px-8 rounded-full text-lg transition duration-300">
-            <Link to="./contact">Contact Us to Get Started</Link>
+            <Link to="./contact">Adhik jaankari ke liye click krein</Link>
           </button>
         </div>
 
