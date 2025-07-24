308748e42bbc2104c94e2755f3dcd13b0f4853a2
Shashikanta Rout <116004580+SkRout8@users.noreply.github.com>
2025-07-24 11:57:46 +0530

    Update NewWorkWithUs.jsx

--- src/components/NewWorkWithUs.jsx
+++ src/components/NewWorkWithUs.jsx
@@ -5,12 +5,13 @@ import RatingWorks from './RatingWorks'
 import Footer from './Footer'
 import { motion } from 'framer-motion'
 import ReelsSection from './ReelsSection'
+import BecomePartner from './BecomePartner'
 
 
 function NewWorkWithUs() {
   return (
     <section>
-    <motion.h2
+      <motion.h2
         className="text-4xl md:text-5xl font-bold text-center  mt-24 "
         initial={{ opacity: 0, y: -30 }}
         animate={{ opacity: 1, y: 0 }}
@@ -19,22 +20,26 @@ function NewWorkWithUs() {
         Work <span className="text-green-800">with us</span>
       </motion.h2>
 
-  
-    <div className=' bg-[#f8f7f0] '>
-    
-    <section className=' mt-20 mr-16 ml-16'>
-        <ReelsSection/>
+
+      <div className=' bg-[#f8f7f0] '>
+
+        <section className=' mt-24'>
+          <BecomePartner />
+        </section>
+
+        <section className=' mt-20 mr-16 ml-16'>
+          <ReelsSection />
+        </section>
+        <section className=' mr-20 ml-20'>
+          <MasonryGallery />
+          <RatingWorks />
+        </section>
+
+        <section className=' mt-20'>
+          <Footer />
+        </section>
+      </div>
     </section>
-       <section className=' mr-20 ml-20'>
-         <MasonryGallery/>
-        <RatingWorks/>
-       </section>
-        
-     <section className=' mt-20'>
-           <Footer/>
-     </section>
-    </div>
-      </section>
   )
 }
 
